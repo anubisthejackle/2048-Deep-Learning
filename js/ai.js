f1e3247a7d28bb84472f1f86e40a3a9a3cf8588b
Travis Weston <anubisthejackle@users.noreply.github.com>
2016-12-19 12:48:04 -0500

    Update ai.js
    Is it possible that keeping too much experience in memory is actually detrimental?

--- js/ai.js
+++ js/ai.js
@@ -4,7 +4,7 @@ function AI() {
 	this.brain = new deepqlearn.Brain(19,4, {
 		epsilon_test_time: 0.0,
 		epsilon_min: 0.01,
-		experience_size: 3000000,
+		//experience_size: 3000000,
 		temporal_window: 100,
 		start_learn_threshold: 1,
 		gama: 0.9
