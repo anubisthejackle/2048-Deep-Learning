6bd9e2031ec085687e07b694bc86ee2af2755a13
Travis Weston <anubisthejackle@users.noreply.github.com>
2017-10-28 10:37:06 -0400

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -100,6 +100,10 @@ AI.prototype.setMoved = function(moved){
 
 AI.prototype.reward = function(meta) {
 
+	if( this.over && !this.won ){
+		this.brain.backward( -1 );
+		return;
+	}
         /*reward = this.getEmptyCount();
 	if( reward > 0 ){
 		reward = reward / 12;
