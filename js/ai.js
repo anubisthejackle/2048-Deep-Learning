1a230fe7d8796ec915221fa3770595c9d5c32743
Travis Weston <anubisthejackle@users.noreply.github.com>
2019-03-18 10:33:00 -0400

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -101,17 +101,17 @@ AI.prototype.setMoved = function(moved){
 AI.prototype.reward = function(meta) {
 
 	if( this.over && !this.won ){
-	//	this.brain.backward( -1 );
+		this.brain.backward( -1 );
 		return;
 	}
-        /*reward = this.getEmptyCount();
+        reward = this.getEmptyCount();
 	if( reward > 0 ){
 		reward = reward / 12;
-	}*/
-	reward = this.getMaxVal() / 2048;
+	}
+	reward += this.getMaxVal() / 2048;
 	
 	if( this.moved == false ){
-		//reward = reward * -1;
+	//	reward = reward * -1;
 		return;
 	}
 	
